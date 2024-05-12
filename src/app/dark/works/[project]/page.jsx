977c562fea7f7e0dfef8756a83b8dfd659c9d99d
HamzaHamani <hamzahamani2004@gmail.com>
2024-05-12 14:28:46 +0100

    chore: Update next.config.js and remove unused files

--- src/app/dark/works/[project]/page.jsx
+++ src/app/dark/works/[project]/page.jsx
@@ -1,12 +1,12 @@
-import ProgressScroll from '@/components/Common/ProgressScroll';
-import Cursor from '@/components/Common/cusor';
-import LoadingScreen from '@/components/Common/loader';
-import Footer from '@/components/dark/blogs/footer';
-import Nav from '@/components/dark/blogs/nav';
-import ContactUs from '@/components/dark/contact/ContactUs';
-import ProjectView from '@/components/dark/works/single-project.jsx/project-view';
-import Script from 'next/script';
-import React from 'react';
+import ProgressScroll from "@/components/Common/ProgressScroll";
+import Cursor from "@/components/Common/cusor";
+import LoadingScreen from "@/components/Common/loader";
+import Footer from "@/components/dark/blogs/footer";
+import Nav from "@/components/dark/blogs/nav";
+import ContactUs from "@/components/dark/contact/ContactUs";
+import ProjectView from "@/components/dark/works/single-project.jsx/project-view";
+import Script from "next/script";
+import React from "react";
 
 function SingleProject() {
   return (
