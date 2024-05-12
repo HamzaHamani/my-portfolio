0a38626e73db04937933f10db909f1c30a35a497
HamzaHamani <hamzahamani2004@gmail.com>
2024-05-12 13:52:06 +0100

    first commit

---
+++ src/components/dark/home/profile.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <section className="intro-profile md-mb50">
+      <div className="row rest">
+        <div className="col-lg-4 box-img main-bg">
+          <div className="cont valign">
+            <div className="full-width">
+              <div className="img">
+                <img src="/assets/imgs/header/profile.jpeg" alt="my picture" />
+                <span className="icon">
+                  <img
+                    src="/assets/imgs/header/icon1.svg"
+                    alt="postman svg icon"
+                  />
+                </span>
+                <span className="icon">
+                  <img
+                    src="/assets/imgs/header/icon2.svg"
+                    alt="react svg icon"
+                  />
+                </span>
+                <span className="icon">
+                  <img
+                    src="/assets/imgs/header/icon4.svg"
+                    alt="next js svg icon"
+                  />
+                </span>
+              </div>
+              <div className="info text-center mt-30">
+                <h5>Hamza Hamani</h5>
+                <p className="fz-13 text-u">Available For Freelance</p>
+              </div>
+              <div className="social text-center mt-20">
+                <a href="https://github.com/HamzaHamani">
+                  <i className="fab fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/hamzahamani/">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-8 content main-bg">
+          <h1 className="">
+            Hello, I’m <span className="main-color">Hamza Hamani</span>, Full
+            Stack Developer and Based in Morocco.
+          </h1>
+          <div className="stauts mt-80">
+            <div className="d-flex align-items-center">
+              <div className="mr-40">
+                <div className="d-flex align-items-center">
+                  <h2>3</h2>
+                  <p>
+                    Years <br /> of Experance
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <div className="butn-presv">
+                  <a href="#0" className="butn butn-md butn-bord radius-5 skew">
+                    <span>Dwonload C.V</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Profile;
