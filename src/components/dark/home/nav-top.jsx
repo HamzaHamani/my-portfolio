7149035c1203f6af0008bcf34abdfadbdff0d0a9
HamzaHamani <hamzahamani2004@gmail.com>
2024-05-14 17:02:38 +0100

    issue with navbar on mobile

--- src/components/dark/home/nav-top.jsx
+++ src/components/dark/home/nav-top.jsx
@@ -32,24 +32,15 @@ function NavTop() {
               </div>
             </div>
           </div>
+
           <div className="col-md-4 valign">
-            {/* <div className="social text-center full-width">
-              <a href="https://github.com/HamzaHamani" target="_blank">
-                <i className="fab fa-github"></i>
-              </a>
-              <a
-                href="https://www.linkedin.com/in/hamzahamani/"
-                target="_blank"
-              >
-                <i className="fab fa-linkedin-in"></i>
-              </a>
-              <a
-                href="https://api.whatsapp.com/send?phone=17604579224"
-                target="_blank"
-              >
-                <i className="fab fa-whatsapp"></i>
-              </a>
-            </div> */}
+            <div className="full-width info">
+              <div className="d-flex justify-content-end">
+                <a href="mailto:haamzahaamani@gmail.com">
+                  <span className="sub-title fz-12"></span>
+                </a>
+              </div>
+            </div>
           </div>
           <div className="col-md-4 valign">
             <div className="full-width info">
